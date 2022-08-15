28762d0a2f5fa1d730f564003783e690b5346247
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-15 16:47:40 +0300

    feat: implement /profile

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -1,19 +1,32 @@
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-import { Header, Title, Subtitle } from '../../css-in-js/global';
-import { getAccount } from '../../services/account';
-import { getToken } from '../../services/cookies';
+import { Header, Title, Subtitle, InputStyle, Link } from '../../css-in-js/global';
+import {
+  changeAuthorNameAccount,
+  changeAuthorUrlAccount,
+  changeShortNameAccount,
+  getAccount,
+} from '../../services/account';
 import { ErrorMessage } from '../../helpers/interfaces';
+import { ErrorPin, InformationPin, PinContainer } from '../Start/start.style';
+import { IconPencil } from './components/IconPencil';
+import { IconApprove } from './components/IconApprove';
+import { Button, Content, EditForm, EmptyMessage, UserData, UserDataItem } from './Profile.style';
+
+type UserInfo = {
+  short_name: string;
+  author_name: string;
+  author_url: string;
+  auth_url: string;
+  page_count: number;
+};
 
 export const Profile = () => {
-  const [userInfo, setUserInfo] = useState({
-    short_name: '',
-    author_name: '',
-    author_url: '',
-    auth_url: '',
-    page_count: 0,
-  });
-  const token = getToken();
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [shortNameEdit, setShortNameEdit] = useState(false);
+  const [authorNameEdit, setAuthorNameEdit] = useState(false);
+  const [authorUrlEdit, setAuthorUrlEdit] = useState(false);
 
   useEffect(() => {
     const get = async () => {
@@ -26,37 +39,191 @@ export const Profile = () => {
     get();
   }, []);
 
+  const {
+    register,
+    unregister,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<ShortNameData>();
+
+  type ShortNameData = {
+    short_name: string;
+    author_name: string;
+    author_url: string;
+  };
+
+  const onSubmitShortName = async (data: ShortNameData) => {
+    if (!userInfo) return;
+
+    if ('short_name' in data) {
+      const { short_name } = data;
+      const response = await changeShortNameAccount(short_name);
+      if (response.ok) setUserInfo({ ...userInfo, short_name });
+      if (!response.ok) setError('short_name', { type: 'server_error', message: 'server error' });
+
+      setShortNameEdit(false);
+    }
+
+    if ('author_name' in data) {
+      const { author_name } = data;
+      const response = await changeAuthorNameAccount(author_name);
+      if (response.ok) setUserInfo({ ...userInfo, author_name });
+      if (!response.ok) setError('author_name', { type: 'server_error', message: 'server error' });
+
+      setAuthorNameEdit(false);
+    }
+
+    if ('author_url' in data) {
+      const { author_url } = data;
+      const response = await changeAuthorUrlAccount(author_url);
+      if (response.ok) {
+        setUserInfo({ ...userInfo, author_url });
+        setAuthorUrlEdit(false);
+        unregister('author_url');
+      }
+      if (!response.ok) {
+        if (response.error === 'AUTHOR_URL_INVALID') {
+          setError('author_url', {
+            type: 'invalid_url',
+            message: 'Invalid url',
+          });
+        } else {
+          setError('author_url', { type: 'server_error', message: 'server error' });
+        }
+      }
+    }
+  };
+
+  console.log(userInfo);
+
   return (
     <>
       <Header>
         <Title>Профиль</Title>
-        <Subtitle>изменить данные</Subtitle>
+        <Subtitle>профиль</Subtitle>
       </Header>
 
-      <main>
-        <label>
-          <p>short_name</p>
-          <input value={userInfo['short_name']} type="text" name="" id="" />
-        </label>
-        <label>
-          <p>author_name</p>
-          <input value={userInfo['author_name']} type="text" name="" id="" />
-        </label>
-        <label>
-          <p>author_url</p>
-          <a href={userInfo['author_url']}>author_url</a>
-        </label>
-        <label>
-          <p>auth_url</p>
-          <a href={userInfo.auth_url}>auth_url</a>
-        </label>
-        <label>
-          <p>page_count {userInfo.page_count}</p>
-        </label>
-        <label>
-          <p>token {token}</p>
-        </label>
-      </main>
+      <UserData>
+        <UserDataItem>
+          {shortNameEdit ? (
+            <EditForm onSubmit={handleSubmit(onSubmitShortName)}>
+              <InputStyle
+                type="text"
+                defaultValue={userInfo?.short_name}
+                {...register('short_name', { required: true, minLength: 3, maxLength: 20 })}
+              />
+              <Button>
+                <IconApprove />
+              </Button>
+            </EditForm>
+          ) : (
+            <Content>
+              <Button onClick={() => setShortNameEdit(true)}>
+                <IconPencil />
+              </Button>
+              <p>short_name: </p>
+              <p>{userInfo?.short_name}</p>
+            </Content>
+          )}
+
+          <PinContainer>
+            {errors.short_name?.type === 'server_error' && <ErrorPin>ошибка сервера</ErrorPin>}
+            {errors.short_name?.type === 'required' && (
+              <ErrorPin>поле должно быть заполнено</ErrorPin>
+            )}
+            {errors.short_name?.type === 'minLength' && (
+              <ErrorPin>минимальная длина 3 символа</ErrorPin>
+            )}
+            {errors.short_name?.type === 'maxLength' && (
+              <ErrorPin>максимальная длина 20 символов</ErrorPin>
+            )}
+            <InformationPin>видно только тебе</InformationPin>
+          </PinContainer>
+        </UserDataItem>
+
+        <UserDataItem>
+          {authorNameEdit ? (
+            <EditForm onSubmit={handleSubmit(onSubmitShortName)}>
+              <InputStyle
+                type="text"
+                defaultValue={userInfo?.author_name}
+                {...register('author_name', { minLength: 3, maxLength: 20 })}
+              />
+              <Button>
+                <IconApprove />
+              </Button>
+            </EditForm>
+          ) : (
+            <Content>
+              <Button onClick={() => setAuthorNameEdit(true)}>
+                <IconPencil />
+              </Button>
+              <p>author_name: </p>
+              <p>{userInfo?.author_name}</p>
+              <EmptyMessage>{userInfo?.author_name === '' ? 'пусто' : ''}</EmptyMessage>
+            </Content>
+          )}
+
+          <PinContainer>
+            {errors.author_name?.type === 'server_error' && <ErrorPin>ошибка сервера</ErrorPin>}
+            {errors.author_name?.type === 'minLength' && (
+              <ErrorPin>минимальная длина 3 символа</ErrorPin>
+            )}
+            {errors.author_name?.type === 'maxLength' && (
+              <ErrorPin>максимальная длина 20 символов</ErrorPin>
+            )}
+            <InformationPin>может быть пустым</InformationPin>
+            <InformationPin>видно под заголовком статьи</InformationPin>
+            <InformationPin>изменится только в новых статьях</InformationPin>
+          </PinContainer>
+        </UserDataItem>
+
+        <UserDataItem>
+          {authorUrlEdit ? (
+            <EditForm onSubmit={handleSubmit(onSubmitShortName)}>
+              <InputStyle
+                type="text"
+                defaultValue={userInfo?.author_url}
+                {...register('author_url', { minLength: 3, maxLength: 20 })}
+              />
+              <Button>
+                <IconApprove />
+              </Button>
+            </EditForm>
+          ) : (
+            <Content>
+              <Button onClick={() => setAuthorUrlEdit(true)}>
+                <IconPencil />
+              </Button>
+              <p>author_url: </p>
+              <p>{userInfo?.author_url}</p>
+              <EmptyMessage>{userInfo?.author_url === '' ? 'пусто' : ''}</EmptyMessage>
+            </Content>
+          )}
+
+          <PinContainer>
+            {errors.author_url?.type === 'server_error' && <ErrorPin>ошибка сервера</ErrorPin>}
+            {errors.author_url?.type === 'invalid_url' && <ErrorPin>неверный формат url</ErrorPin>}
+            {errors.author_url?.type === 'minLength' && (
+              <ErrorPin>минимальная длина 3 символа</ErrorPin>
+            )}
+            {errors.author_url?.type === 'maxLength' && (
+              <ErrorPin>максимальная длина 20 символов</ErrorPin>
+            )}
+            <InformationPin>может быть пустым</InformationPin>
+            <InformationPin>author_name станет ссылкой</InformationPin>
+            <InformationPin>изменится, только в новых статьях</InformationPin>
+            <InformationPin>должно начинаться с http:// или https://</InformationPin>
+          </PinContainer>
+        </UserDataItem>
+
+        <Link href="/token">Управление токеном</Link>
+
+        <Link href={userInfo?.auth_url} target="_blank">
+          Авторизоваться в telegra.ph
+        </Link>
+      </UserData>
     </>
   );
 };
