9bf78e73638dcaa3167d9133cb1b0e9cfd65d015
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-18 13:55:47 +0300

    feat: implement /page

--- src/views/Page/Page.tsx
+++ src/views/Page/Page.tsx
@@ -1,8 +1,22 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
+import { Header, Title, Subtitle, Link } from '../../css-in-js/global';
 import { PageObject } from '../../helpers/interfaces';
 import { getPage } from '../../services/page';
+
+const PageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  word-break: break-all;
+`;
+
+const PageItemTitle = styled.span`
+  font-weight: 600;
+  padding-right: 8px;
+`;
 
 export function Page() {
   const pathPage = useParams().id;
@@ -19,17 +33,56 @@ export function Page() {
     get();
   }, [pathPage]);
 
+  const cutTitle = (title: string | undefined) => {
+    if (!title) return;
+    const len = title.length;
+
+    const clientWidth = document.documentElement.clientWidth;
+    const workPlace = clientWidth > 730 ? 730 : clientWidth;
+    const paddings = 42;
+
+    const countChar = Math.ceil((workPlace - paddings) / 18);
+
+    const eclipse = '...';
+    if (len > countChar) return title.slice(0, countChar - eclipse.length) + eclipse;
+    return title;
+  };
+
   return (
     <>
-      <p>views: {page?.views}</p>
-      <h1>{page?.title}</h1>
-      <address>
-        <span>author name:</span> {page?.author_name}
-        <br />
-        <span>author url:</span> {page?.author_url}
-      </address>
-      <p>{page?.description}</p>
-      <a href={page?.url}>{page?.path}</a>
+      <Header>
+        <Title>{cutTitle(page?.title)}</Title>
+        <Subtitle>Просмотров: {page?.views}</Subtitle>
+      </Header>
+
+      <PageContent>
+        <p>
+          <PageItemTitle>title:</PageItemTitle>
+          {page?.title}
+        </p>
+
+        <p>
+          <PageItemTitle>author_name:</PageItemTitle>
+          {page?.author_name}
+        </p>
+
+        <p>
+          <PageItemTitle>author_url:</PageItemTitle>
+          {page?.author_url}
+        </p>
+
+        <p>
+          <PageItemTitle>description:</PageItemTitle>
+          {page?.description}
+        </p>
+
+        <p>
+          <PageItemTitle>link:</PageItemTitle>
+          <Link href={page?.url} target="_blank">
+            {page?.path}
+          </Link>
+        </p>
+      </PageContent>
     </>
   );
 }
