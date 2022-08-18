20829e426f5130fcac4b42f3967920ade9d6d8f9
u1f5a4 <73600007+u1f5a4@users.noreply.github.com>
2022-08-18 13:50:54 +0300

    feat: add .safari_only

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,7 +7,7 @@ const NavStyled = styled.nav`
   width: 100%;
   max-width: 730px;
 
-  margin-top: 12px;
+  margin-bottom: 21px;
   margin-left: auto;
   margin-right: auto;
   padding: 0 21px;
@@ -29,15 +29,14 @@ export const Nav = (props: NavProps) => {
   const { isAuth } = props;
 
   return (
-    <NavStyled>
+    <NavStyled className="safari_only">
       <NavLink to="/">Главная</NavLink>
 
       {isAuth ? (
         <>
-          <NavLink to="/pages">Статьи</NavLink>
-          <NavLink to="/profile">Профиль</NavLink>
+          <NavLink to="/pages">Страницы</NavLink>
+          <NavLink to="/profile">Аккаунт</NavLink>
           <NavLink to="/logout">Выйти</NavLink>
-
         </>
       ) : (
         <NavLink to="/start">Старт</NavLink>
